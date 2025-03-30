cec0673564bfabcda29ae8384e28b82886fa3aa2
oliveirashaiana608 <oliveirashaiana608@gmail.com>
2025-03-30 14:13:12 +0000

    Add 21.php

---
+++ src/21.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>GitHub School Project</title>
+</head>
+<body>
+    <h1>Welcome to GitHub School!</h1>
+    <p>This is a simple PHP project inspired by the GitHub school framework. Feel free to edit and modify this code to suit your needs.</p>
+    <footer>
+        <a href="https://github.com/microsoft">GitHub</a> | 
+        <a href="https://www.microsoft.com/en-us/playground">Playground</a>
+    </footer>
+</body>
+</html>
